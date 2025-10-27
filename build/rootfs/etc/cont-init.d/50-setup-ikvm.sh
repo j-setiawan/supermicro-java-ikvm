3cb576513d574817c77e5aaa7389f6d3660bb396
Julian Setiawan <julian.setiawan@gmail.com>
2025-10-27 13:44:56 -0400

    Use new login method

--- build/rootfs/etc/cont-init.d/50-setup-ikvm.sh
+++ build/rootfs/etc/cont-init.d/50-setup-ikvm.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
 # ipmikvm-tls2020 (part of ossobv/vcutil) // wdoekes/2020 // Public Domain
 #
+# MODIFIED to support Supermicro BMCs that use a complex JS-based download trigger.
+# Final version based on decompiled JS analysis.
+#
 # A wrapper to call the SuperMicro iKVM console bypassing Java browser
 # plugins.
 #
 # Requirements: base64, curl, java
 #
-# Usage:
-#
-#   $ ipmikvm-tls2020
-#   Usage: ipmikvm-tls2020 [-u ADMIN] [-P ADMIN] IP.ADD.RE.SS
-#
-#   $ ipmikvm-tls2020 10.11.12.13 -P otherpassword
-#   (connects KVM console on IPMI device at 10.11.12.13)
-#
-# This has been tested with iKVM__V1.69.39.0x0.
-#
-# See also: ipmikvm
-#
 set -e # Exit immediately if a command exits with a non-zero status
 set -u # Treat unset variables as an error
-set -x
 
-APP_CACHE_DIR=$XDG_CACHE_HOME
+APP_CACHE_DIR=${XDG_CACHE_HOME:-/root/.cache}/ikvm
 
 get_launch_jnlp() {
     fail=1
     url="http://$KVM_HOST"
     temp=$(mktemp)
-    
+
     if curl --fail -sk --cookie-jar "$temp" -XPOST "$url/login.asp" \
-          --data "name=$KVM_USER&pwd=$KVM_PASS&check=00" -o/dev/null; then
+          --data "name=$KVM_USER&pwd=$KVM_PASS" -o/dev/null; then
+
+        JNLP_URL="$url/Java/jviewer.jnlp"
+        REFERER_URL="$url/page/main.html" 
+
+        echo "Attempting to download JNLP from $JNLP_URL" >&2
+
         launch_jnlp=$(curl --fail -sk --cookie "$temp" \
-            --referer "$url/cgi/url_redirect.cgi?url_name=man_ikvm" \
-            "$url/cgi/url_redirect.cgi?url_name=man_ikvm&url_type=jwsk")
-        test $? -eq 0 && fail=
+            --referer "$REFERER_URL" \
+            "$JNLP_URL")
+
+        if [ $? -eq 0 ] && echo "$launch_jnlp" | grep -q "<jnlp"; then
+            fail=""
+            echo "Successfully fetched JNLP content." >&2
+        else
+            echo "Failed to fetch or validate JNLP content from $JNLP_URL" >&2
+        fi
     fi
     rm "$temp"
     test -z "$fail" && echo "$launch_jnlp"
@@ -42,7 +44,7 @@ get_launch_jnlp() {
 
 get_arguments() {
     launch_jnlp="$1"
-    echo "$launch_jnlp" | sed -e '/<argument>/!d;s#.*<argument>\([^<]*\)</argument>.*#\1#' | 
+    echo "$launch_jnlp" | sed -e '/<argument>/!d;s#.*<argument>\([^<]*\)</argument>.*#\1#' |
       sed -e "s/['\"$]//g;s/.*/&/" | sed -e 1,4d
 }
 
@@ -62,7 +64,6 @@ get_app_class() {
     echo "$1" | sed -ne 's/.*<application-desc .*main-class="\([^"]*\)".*/\1/p'
 }
 
-# SYNOPSIS: get_app_class JNLP_DATA
 install_ikvm_application() {
     launch_jnlp="$1"
     destdir="$2"
@@ -89,21 +90,21 @@ install_ikvm_application() {
     set +e
 }
 
-JNLP=$(get_launch_jnlp "$KVM_HOST" "$KVM_USER" "$KVM_PASS")
+JNLP=$(get_launch_jnlp)
 test -z "$JNLP" && echo "Failed to get launch.jnlp" >&2 && exit 1
 
-JAR=$(find $APP_CACHE_DIR -name 'iKVM*.jar' | sort | tail -n1)
+JAR=$(find "$APP_CACHE_DIR" -name 'iKVM*.jar' | sort | tail -n1)
 if ! test -f "$JAR"; then
     install_ikvm_application "$JNLP" "$APP_CACHE_DIR"
-    JAR=$(find $APP_CACHE_DIR -name 'iKVM*.jar' | sort | tail -n1)
-    if ! ls -l "$JAR"; then
+    JAR=$(find "$APP_CACHE_DIR" -name 'iKVM*.jar' | sort | tail -n1)
+    if ! test -f "$JAR"; then
         echo "Install failure" >&2
         exit 1
     fi
 fi
 
-echo $JAR > /etc/cont-env.d/KVM_JAR_FILE
-echo $(get_username "$JNLP") > /etc/cont-env.d/KVM_EPHEMERAL_USERNAME
-echo $(get_password "$JNLP") > /etc/cont-env.d/KVM_EPHEMERAL_PASSWORD
-echo $(get_app_class "$JNLP") > /etc/cont-env.d/KVM_JAR_APPCLASS
-echo $(get_arguments "$JNLP") > /etc/cont-env.d/KVM_LAUNCH_ARGUMENTS
+echo "$JAR" > /etc/cont-env.d/KVM_JAR_FILE
+echo "$(get_username "$JNLP")" > /etc/cont-env.d/KVM_EPHEMERAL_USERNAME
+echo "$(get_password "$JNLP")" > /etc/cont-env.d/KVM_EPHEMERAL_PASSWORD
+echo "$(get_app_class "$JNLP")" > /etc/cont-env.d/KVM_JAR_APPCLASS
+echo "$(get_arguments "$JNLP")" > /etc/cont-env.d/KVM_LAUNCH_ARGUMENTS
